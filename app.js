561c908d40bbac6dbb06a9c3c127c40655545e95
Abdul Rahiman Shaik <sabihasheerin.s@gmail.com>
2021-09-02 17:59:01 +0400

    console

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log('hey this is my first piece of js code !!! YAY')
+
+
+console.log(1268990936286)
+
+console.log({ name: "yutika"})
+
+console.log(true)
+console.log(false)
+console.log(false,23423, 'string',{name:"sabiha"})
+console.warn("This is a warning")
+console.error("this is an error")
