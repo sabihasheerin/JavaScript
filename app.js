799c9ef3f3b218cf937e449858fdb0a97421dec9
Abdul Rahiman Shaik <sabihasheerin.s@gmail.com>
2021-09-03 18:08:16 +0400

    new arithmetic operators

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
-let Name = "abdul"
+// Arithmetic operators
 
-console.log(Name.toUpperCase())
+const a = 5
+const b = 15
 
+console.log(a+b)
+
+console.log(a-b)
+  
+console.log(a*b)
+
+console.log(a/b)
+
+console.log(a**b)
+
+console.log(b%a)
+
+console.log('-------------------')
+
+
+let c = 5 
+ 
+c **=3
+//c operator=number is equivalent to c = c operator number
+
+// c = c * 5=> 5*5 = 25
+
+
+console.log(c)
