21655e64caf51c127fb1d26ac6d16288d94739b0
Abdul Rahiman Shaik <sabihasheerin.s@gmail.com>
2021-09-04 18:00:33 +0400

    elements

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-//const element = document.getElementById('selectme')
-
-
-//const element = document.getElementByIdClassName('heading')
-
-//const element = document.getElementByTagName('h2')
-
 const element = document.querySelector('#selectme')
+//element.innerHTML = "This is from JS"
+
+// element.style.color = "cyan"
+//element.style.fontsize = "40px"
+//element.style = 'display:none'
+
+//console.log(element.classList)
+//element.classList.add('five')
+//console.log(element.classList)
+//element.classList.remove('one')
+//console.log(element.classList)
+
+const value = element.classList.contains('three')
 
 
-
-
-
-console.log(element)
+console.log(value)
