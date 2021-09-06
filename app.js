f842d91aa8b6215ec9e4f616f51a6277db32c58a
Abdul Rahiman Shaik <sabihasheerin.s@gmail.com>
2021-09-06 17:18:25 +0400

    es-6functions

--- app.js
+++ app.js
@@ -2,25 +2,15 @@
 // const paper = document.querySelector('#paper')
 // const scissor = document.querySelector('#scissor')
 
-// var.Name
-
-//console.log(Name)
 
 function logName(Name = "john doe ", age = 18){
   //console.log("sabiha")
-   console.log(Name + " you are" + age)
+ console.log(Name + " you are" + age)
  }
 
-// Never Misspell the function keyword
-//a function can have multiple parameters
-//you can set default values for the parameters
-// Any variable you define inside a functions body is local to that function
-
-function add(a = 0 , b = 0,){
-   
- return a + b
+const add = (a = 0,b = 0) => {
+   return a + b
 }
 
-const result = add()
+console.log(add(12,13))
 
-console.log(result)
