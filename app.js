2a4915a4c89803f5aac1dc850f6c0d5143cf1d7b
Abdul Rahiman Shaik <sabihasheerin.s@gmail.com>
2021-09-07 16:53:45 +0400

    arrays-DOM

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
- const balls = document.querySelectorAll('.ball')
+const balls = document.querySelectorAll('.ball')
 //const balls = document.getElementByClassName('ball')
 const body = document.querySelector('body')
 
-const arr = [1 ,2, 3, 4, 5]
-//for (var i =0;i < arr.length; i++){
-   // console.log(arr[i])  
-// }
-
-arr.forEach((element,iteration) => { 
-console.log('element,iteration')
+balls.forEach((ball, iteration) => {
+    console.log(iteration)
+    const value = (iteration + 1) * 50
+    ball.style = `transform:translateY(${value}px)`
 
 })
+
+let age = 19
+let Name = "sabiha"
+
+console.log('your age is' + age)
+console.log('hi ${name} your age  is ${age}')
