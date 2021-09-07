d791a6ffa1ba9d7ccb5492fe7c88e4436c6b0d73
Abdul Rahiman Shaik <sabihasheerin.s@gmail.com>
2021-09-07 16:37:14 +0400

    array-methods

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-// const balls = document.querySelectorAll('.ball')
-const balls = document.getElementByClassName('.ball')
+ const balls = document.querySelectorAll('.ball')
+//const balls = document.getElementByClassName('ball')
 const body = document.querySelector('body')
-console.log(balls.push)
+
+const arr = [1 ,2, 3, 4, 5]
+//for (var i =0;i < arr.length; i++){
+   // console.log(arr[i])  
+// }
+
+arr.forEach((element,iteration) => { 
+console.log('element,iteration')
+
+})
